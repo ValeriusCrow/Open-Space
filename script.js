7b74731a36ebc5a082ad8ef59f5049eba97ffbe8
ValeriusCrow <77670739+ValeriusCrow@users.noreply.github.com>
2021-05-23 21:43:19 +0300

    "Launch" unlocked by checking all checkboxes

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let elList = document.querySelectorAll("input");
-    for (let i=2; i < elList.length; i++) {
-        elList[i].setAttribute("disabled", "")}
-
 let passwordField = document.querySelector('input[type="password"]');
 let okButton = document.querySelector('input[value="OK"]');
+let launchButton = document.querySelector('input[value="Launch"]');
+let rangesChecks = document.querySelectorAll('input[type="checkbox"], input[type="range"]');
+
+for (let i=2; i < elList.length; i++) {
+    elList[i].setAttribute("disabled", "")}
 
 okButton.addEventListener("click", function unlock () {
     if (passwordField.value === "TrustNo1") {
-        for (let i=2; i < elList.length; i++) {
+        for (let i=2; i < elList.length - 1; i++) {
             elList[i].removeAttribute("disabled")}
     }
 });
+
+
+for (let i = 0; i <= rangesChecks.length; i++) {
+    rangesChecks[i].addEventListener("click", function check() {
+        let checkedChecks = document.querySelectorAll('input[type="checkbox"]:checked');
+        let rangesFull = document.querySelectorAll('input[type="range"], input[value="100"]');
+        if (checkedChecks.length === 6) {
+            launchButton.removeAttribute("disabled");
+        } else {
+            launchButton.setAttribute("disabled", "");
+        }
+    });
+}
